dd603a54d3a4d4e1729f9ed15c05fd1ccd7320aa
Ahmad Mansour <ahmad.mansour92@outlook.com>
2020-01-12 02:55:44 +0400

    Adding Location permession with Marker and Location Change Listner to GoogleMaps RiderActivity

--- app/src/main/java/com/ahmadmansour/startapp/RaiderActivity.kt
+++ app/src/main/java/com/ahmadmansour/startapp/RaiderActivity.kt
@@ -1,7 +1,15 @@
 package com.ahmadmansour.startapp
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,6 +21,7 @@ import com.google.android.gms.maps.model.MarkerOptions
 class RaiderActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private var accessLocationRequestCode = 200
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +30,32 @@ class RaiderActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.rider_map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        if(Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager
+                            .PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                        accessLocationRequestCode)
+                return
+            }
+        }
+        // continue because you have permission
+       getCurrentLocation()
+    }
+
+    private fun getCurrentLocation() {
+        Toast.makeText(this.baseContext, "user enabled the location permession", Toast
+                .LENGTH_LONG).show()
+        var locationOnMap = mapLocationListener(this.baseContext)
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3,
+                3f, locationOnMap)
+
     }
 
     /**
@@ -36,9 +71,47 @@ class RaiderActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         //25.2048° N, 55.2708° E
-        // Add a marker in Sydney and move the camera
+        // Add a marker in Dubai and move the camera
         val dubai = LatLng(25.2048, 55.2708)
-        mMap.addMarker(MarkerOptions().position(dubai).title("Marker in dubai"))
+        mMap.addMarker(MarkerOptions().position(dubai).title("new Location"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(dubai))
     }
+
+    var location: Location? = null
+
+    inner class mapLocationListener: LocationListener {
+        var context: Context?= null
+
+        constructor(context: Context) {
+            location = Location("Start")
+            location!!.latitude = 0.0
+            location!!.longitude = 0.0
+            this.context = context
+        }
+
+        override fun onLocationChanged(location: Location?) {
+            Toast.makeText(context, "user location has been changed" + location!!.latitude + ", "
+                    + location!!.longitude, Toast
+                    .LENGTH_LONG).show()
+            mMap.clear()
+
+            val dubai = LatLng(location!!.latitude, location!!.longitude)
+            mMap.addMarker(MarkerOptions().position(dubai).title("New Location, 00971506708115"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(dubai))
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+        }
+
+        override fun onProviderEnabled(provider: String?) {
+
+        }
+
+        override fun onProviderDisabled(provider: String?) {
+
+        }
+
+    }
+
 }
