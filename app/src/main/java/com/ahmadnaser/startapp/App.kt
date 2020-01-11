c838dad4610df9468d0423dfc545c88bdd7afe78
Ahmad Mansour <ahmad.mansour92@outlook.com>
2020-01-11 16:52:08 +0400

    Uber App StartAapp initial

---
+++ app/src/main/java/com/ahmadnaser/startapp/App.kt
@@ -0,0 +1,50 @@
+package com.ahmadnaser.startapp
+
+
+import android.app.Application
+    import android.util.Log
+    import com.parse.*
+
+
+    class App : Application() {
+
+        override fun onCreate() {
+            super.onCreate()
+
+    //3
+            // Enable Local Datastore.
+            Parse.enableLocalDatastore(this)
+
+            //1
+        //    ParseObject.registerSubclass(User::class.java)
+/*//    //2
+            Parse.initialize(Parse.Configuration.Builder(this)
+                    .applicationId(BuildConfig.APP_ID)
+                    .clientKey(BuildConfig.CLIENT_KEY)
+                    .server(BuildConfig.SERVER_URL)
+                    .build()
+            )
+*/
+    //4-a Create Example on parse name,phone
+    //4-b Use the following code to inser data
+            val obj = ParseObject("Example")
+            obj.put("name","Ahmad")
+            obj.put("phone","00970599042502")
+
+
+
+    //5
+            ParseUser.enableAutomaticUser()
+    //6
+            val defaultACL = ParseACL()
+            defaultACL.publicReadAccess = true
+            defaultACL.publicWriteAccess = true
+            ParseACL.setDefaultACL(defaultACL, true)
+
+
+
+
+
+        }
+
+    }
