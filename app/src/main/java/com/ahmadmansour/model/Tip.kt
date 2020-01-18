a6d03fcf455a38f618a642b046626c9be686a34f
Ahmad Mansour <ahmad.mansour92@outlook.com>
2020-01-18 22:59:28 +0400

    ADDING THE DAO CLASS TO DEAL WITH THE PARSE SERVER DATABSE

--- app/src/main/java/com/ahmadmansour/model/Tip.kt
+++ app/src/main/java/com/ahmadmansour/model/Tip.kt
@@ -8,10 +8,19 @@ class Tip {
     var tipId: Int? = null
     var tipName: String? = null
     var tipDescription: String? = null
+    var objectId: String? = null
+    var timeStamp: Long = 0
+
     constructor(id: Int, n: String, d: String) {
         this.tipId = id
         this.tipName = n
         this.tipDescription = d
     }
 
+    constructor(id: Int, n: String, d: String, objectId: String) {
+        this.tipId = id
+        this.tipName = n
+        this.tipDescription = d
+        this.objectId = objectId
+    }
 }
